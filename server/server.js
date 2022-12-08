f2090605102e7b1bc683381637151900ea6889da
Reuben Lee <reubenwlee@gmail.com>
2022-12-07 19:43:06 -0500

    testing successful server deployment

--- server/server.js
+++ server/server.js
@@ -17,3 +17,6 @@ app.listen(port, () => {
   });
   console.log(`Server is running on port: ${port}`);
 });
+
+// Confirming Successful Heroku Deployment
+app.get('/', (req, res) => { res.send('Hello from Express!')})
